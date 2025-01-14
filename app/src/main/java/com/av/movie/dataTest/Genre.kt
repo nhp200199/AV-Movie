4df7449b8eb9e4ef79a9dc39f67c860a9811c25f
Dennis Nguyen <nhp200199@gmail.com>
2025-01-14 16:19:02 +0700

    Onboarding UI

--- app/src/main/java/com/av/movie/dataTest/Genre.kt
+++ app/src/main/java/com/av/movie/dataTest/Genre.kt
@@ -2,44 +2,44 @@ package com.av.movie.dataTest
 
 import com.av.movie.presentation.screen.home.Genre
 
-val MODEL_ACTION = Genre(id = 28, name = "Action")
-val MODEL_ADVENTURE = Genre(id = 12, name = "Adventure")
-val MODEL_ANIMATION = Genre(id = 16, name = "Animation")
-val MODEL_COMEDY = Genre(id = 35, name = "Comedy")
-val MODEL_CRIME = Genre(id = 80, name = "Crime")
-val MODEL_DOCUMENTARY = Genre(id = 99, name = "Documentary")
-val MODEL_DRAMA = Genre(id = 18, name = "Drama")
-val MODEL_FAMILY = Genre(id = 10751, name = "Family")
-val MODEL_FANTASY = Genre(id = 14, name = "Fantasy")
-val MODEL_HISTORY = Genre(id = 36, name = "History")
-val MODEL_HORROR = Genre(id = 27, name = "Horror")
-val MODEL_MUSIC = Genre(id = 10402, name = "Music")
-val MODEL_MYSTERY = Genre(id = 9648, name = "Mystery")
-val MODEL_ROMANCE = Genre(id = 10749, name = "Romance")
-val MODEL_SCIENCE_FICTION = Genre(id = 878, name = "Science Fiction")
-val MODEL_TV_MOVIE = Genre(id = 10770, name = "TV Movie")
-val MODEL_THRILLER = Genre(id = 53, name = "Thriller")
-val MODEL_WAR = Genre(id = 10752, name = "War")
-val MODEL_WESTERN = Genre(id = 37, name = "Western")
+val MODEL_GENRE_ACTION = Genre(id = 28, name = "Action")
+val MODEL_GENRE_ADVENTURE = Genre(id = 12, name = "Adventure")
+val MODEL_GENRE_ANIMATION = Genre(id = 16, name = "Animation")
+val MODEL_GENRE_COMEDY = Genre(id = 35, name = "Comedy")
+val MODEL_GENRE_CRIME = Genre(id = 80, name = "Crime")
+val MODEL_GENRE_DOCUMENTARY = Genre(id = 99, name = "Documentary")
+val MODEL_GENRE_DRAMA = Genre(id = 18, name = "Drama")
+val MODEL_GENRE_FAMILY = Genre(id = 10751, name = "Family")
+val MODEL_GENRE_FANTASY = Genre(id = 14, name = "Fantasy")
+val MODEL_GENRE_HISTORY = Genre(id = 36, name = "History")
+val MODEL_GENRE_HORROR = Genre(id = 27, name = "Horror")
+val MODEL_GENRE_MUSIC = Genre(id = 10402, name = "Music")
+val MODEL_GENRE_MYSTERY = Genre(id = 9648, name = "Mystery")
+val MODEL_GENRE_ROMANCE = Genre(id = 10749, name = "Romance")
+val MODEL_GENRE_SCIENCE_FICTION = Genre(id = 878, name = "Science Fiction")
+val MODEL_GENRE_TV_MOVIE = Genre(id = 10770, name = "TV Movie")
+val MODEL_GENRE_THRILLER = Genre(id = 53, name = "Thriller")
+val MODEL_GENRE_WAR = Genre(id = 10752, name = "War")
+val MODEL_GENRE_WESTERN = Genre(id = 37, name = "Western")
 
 val ALL_GENRES = listOf(
-    MODEL_ACTION,
-    MODEL_ADVENTURE,
-    MODEL_ANIMATION,
-    MODEL_COMEDY,
-    MODEL_CRIME,
-    MODEL_DOCUMENTARY,
-    MODEL_DRAMA,
-    MODEL_FAMILY,
-    MODEL_FANTASY,
-    MODEL_HISTORY,
-    MODEL_HORROR,
-    MODEL_MUSIC,
-    MODEL_MYSTERY,
-    MODEL_ROMANCE,
-    MODEL_SCIENCE_FICTION,
-    MODEL_TV_MOVIE,
-    MODEL_THRILLER,
-    MODEL_WAR,
-    MODEL_WESTERN
+    MODEL_GENRE_ACTION,
+    MODEL_GENRE_ADVENTURE,
+    MODEL_GENRE_ANIMATION,
+    MODEL_GENRE_COMEDY,
+    MODEL_GENRE_CRIME,
+    MODEL_GENRE_DOCUMENTARY,
+    MODEL_GENRE_DRAMA,
+    MODEL_GENRE_FAMILY,
+    MODEL_GENRE_FANTASY,
+    MODEL_GENRE_HISTORY,
+    MODEL_GENRE_HORROR,
+    MODEL_GENRE_MUSIC,
+    MODEL_GENRE_MYSTERY,
+    MODEL_GENRE_ROMANCE,
+    MODEL_GENRE_SCIENCE_FICTION,
+    MODEL_GENRE_TV_MOVIE,
+    MODEL_GENRE_THRILLER,
+    MODEL_GENRE_WAR,
+    MODEL_GENRE_WESTERN
 )
---
+++ app/src/main/java/com/av/movie/presentation/screen/onboarding/OnBoardingScreen.kt
@@ -0,0 +1,225 @@
+package com.av.movie.presentation.screen.onboarding
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Icon
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.CheckCircle
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.paint
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.av.avmovie.R
+import com.av.movie.dataTest.ALL_GENRES
+import com.av.movie.dataTest.MODEL_GENRE_ACTION
+import com.av.movie.presentation.screen.home.Genre
+import com.av.movie.ui.theme.Blue90
+import com.av.movie.ui.theme.Grey10
+import com.av.movie.ui.theme.LightGrey30
+
+@Composable
+fun OnBoardingScreen() {
+    Scaffold(
+        topBar = {
+            TopAppBar(title = {
+                Text(text = "Pick what you'd like to watch")
+            })
+        },
+        backgroundColor = Grey10
+    ) {
+        Box(
+            modifier =
+            Modifier.padding(it)
+        ) {
+            var selectedGenres by remember { mutableStateOf(emptyList<Int>()) }
+
+            GenreSelection(
+                genres = ALL_GENRES,
+                modifier = Modifier
+                    .fillMaxWidth(),
+                selectedGenres = selectedGenres,
+                onGenreSelected = { genreId ->
+                    selectedGenres = if (selectedGenres.contains(genreId)) {
+                        selectedGenres.filter { genre -> genre != genreId }
+                    } else {
+                        selectedGenres + genreId
+                    }
+                }
+            )
+
+            FooterAction(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .align(Alignment.BottomCenter)
+                    .height(100.dp),
+                enabled = selectedGenres.isNotEmpty()
+            )
+        }
+    }
+}
+
+@Composable
+fun FooterAction(
+    modifier: Modifier = Modifier,
+    enabled: Boolean = false
+) {
+    Box(
+        modifier = modifier
+            .background(Grey10)
+    ) {
+        Button(
+            onClick = { /*TODO*/ },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 16.dp, horizontal = 32.dp)
+                .background(
+                    color = if (enabled) Blue90 else Color.White,
+                    shape = RoundedCornerShape(10.dp)
+                )
+                .align(Alignment.Center),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color.Transparent,
+                disabledContainerColor = Color.Transparent
+            ),
+            enabled = enabled
+        ) {
+            Text(
+                text = if (enabled) "Done" else "Select at least 1",
+                style = TextStyle(
+                    color = if (enabled) Color.White else Color.Black,
+                    fontSize = 16.sp
+                )
+            )
+        }
+    }
+}
+
+@Composable
+fun GenreSelection(
+    genres: List<Genre>,
+    modifier: Modifier = Modifier,
+    selectedGenres: List<Int> = emptyList(),
+    onGenreSelected: (Int) -> Unit = {}
+) {
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(3),
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+        contentPadding = PaddingValues(8.dp),
+        modifier = modifier
+    ) {
+        items(
+            count = genres.size,
+            key = { genres[it].id }
+        ) {
+            GenreItem(
+                genres[it],
+                modifier = Modifier.fillMaxWidth(),
+                isSelected = selectedGenres.contains(genres[it].id),
+                onGenreSelected = onGenreSelected
+            )
+
+        }
+    }
+}
+
+@Composable
+fun GenreItem(
+    genre: Genre,
+    modifier: Modifier = Modifier,
+    isSelected: Boolean = false,
+    onGenreSelected: (Int) -> Unit = {}
+) {
+    Box(
+        modifier = modifier
+            .paint(
+                painter = painterResource(R.drawable.ic_launcher_background),
+                contentScale = ContentScale.FillWidth,
+                alpha = if (isSelected) 1f else 0.9f
+            )
+            .border(
+                width = 1.dp,
+                color = Blue90
+            )
+            .clickable {
+                onGenreSelected(genre.id)
+            }
+    ) {
+        Box(
+            modifier = Modifier
+                .align(Alignment.Center)
+                .fillMaxWidth(0.9f)
+        ) {
+            Text(
+                text = genre.name,
+                modifier = Modifier
+                    .align(Alignment.Center)
+                    .background(
+                        color = LightGrey30.copy(alpha = 0.3f),
+                        shape = RoundedCornerShape(10.dp)
+                    )
+                    .padding(vertical = 4.dp, horizontal = 8.dp),
+                style = TextStyle(
+                    color = Color.White,
+                    fontSize = 14.sp,
+                    textAlign = TextAlign.Center
+                )
+            )
+        }
+
+        if (isSelected) {
+            Icon(
+                imageVector = Icons.Filled.CheckCircle,
+                contentDescription = "Checked",
+                tint = Blue90,
+                modifier = Modifier
+                    .align(Alignment.TopEnd)
+                    .padding(top = 4.dp, end = 4.dp)
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun OnBoardingScreenPreview() {
+    OnBoardingScreen()
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GenreItemPreview() {
+    GenreItem(
+        genre = MODEL_GENRE_ACTION,
+        modifier = Modifier
+            .width(150.dp)
+    )
+}
