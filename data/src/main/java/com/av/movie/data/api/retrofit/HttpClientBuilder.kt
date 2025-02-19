8fd51773110d8d5d718eab07fd057310c85341aa
Dennis Nguyen <nhp200199@gmail.com>
2025-02-19 15:07:40 +0700

    adding data

--- data/src/main/java/com/av/movie/data/api/retrofit/HttpClientBuilder.kt
+++ data/src/main/java/com/av/movie/data/api/retrofit/HttpClientBuilder.kt
@@ -2,6 +2,7 @@ package com.av.movie.data.api.retrofit
 
 import android.content.Context
 import com.av.movie.data.networkManager.NetworkManager
+import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 
@@ -16,6 +17,11 @@ class HttpClientBuilder {
 
             builder.addInterceptor(logging)
             builder.addInterceptor(networkMonitorInterceptor)
+            builder.addInterceptor(Interceptor { chain ->
+                val newRequestBuilder = chain.request().newBuilder()
+                newRequestBuilder.addHeader("Authentication", "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5NTJjZDU3OTFiYzhmMzE3ZDk2ZWVjZmYxMTU2NTNmMSIsIm5iZiI6MTczNDM0NjkwMi4xMzc5OTk4LCJzdWIiOiI2NzYwMDg5NjVlM2FiZDY4MDZiYjAwMDMiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.MWBjWyMTFLKAEuuZVRpfpep2k9_nWseCH1Ni-dYOqr4")
+                chain.proceed(newRequestBuilder.build())
+            })
 
             return builder
         }
